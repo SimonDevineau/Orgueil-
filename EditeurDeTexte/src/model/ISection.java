bce3e004e129b0acbe982d018e98e830517d5d7e
simon.devineau <simon.devineau@gmail.com>
2012-10-09 18:24:47 +0200

    creation of ISection

---
+++ EditeurDeTexte/src/model/ISection.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ *9 oct. 2012 - EditeurDeTexte.
+ *@author Simon Devineau & Pierre Reliquet
+ *Ecole des Mines de Nantes
+ *Major in Computer and Information System Engineering
+ *ISection.java 
+ */
+public interface ISection {
+}
