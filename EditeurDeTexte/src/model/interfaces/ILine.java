fcf7d8dba8503486c710f5a2ac100007f772e366
Pierre Reliquet <pierre.reliquet@gmail.com>
2012-10-10 09:29:40 +0200

    Adding the methods in the ILine interface

--- EditeurDeTexte/src/model/interfaces/ILine.java
+++ EditeurDeTexte/src/model/interfaces/ILine.java
@@ -2,11 +2,43 @@ package model.interfaces;
 
 /**
  * 10 oct. 2012 - EditeurDeTexte.
+ * 
  * @author Simon Devineau & Pierre Reliquet
- * Ecole des Mines de Nantes
- * Major in Computer and Information System Engineering
- * ILine.java 
+ * Ecole des Mines de Nantes 
+ * Major in Computer and Information System Engineering 
+ * ILine.java
  */
 public interface ILine {
+
+	/**
+	 * @param content
+	 *            , content to append.
+	 */
+	public void append(CharSequence content);
+
+	/**
+	 * Return true if the current ILine has the cursor and false otherwise
+	 * 
+	 * @return a boolean which indicates if the line has the cursor.
+	 */
+	public boolean hasCursor();
+
+	/**
+	 * @return the location of the cursor if the line has the cursor
+	 */
+	public int getCursorLocation();
+
+	/**
+	 * @return the String representation of the ILine
+	 */
+	@Override
+	public String toString();
+	
+	/**
+	 * @param obj, the object which should be tester for equality check
+	 * @return true if <i><b>this</b></i> and obj are equal
+	 */
+	@Override
+	public boolean equals(Object obj);
 
 }
