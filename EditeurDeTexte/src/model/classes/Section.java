31098f04c9f73a04300b845a9e1ad7231d36c667
simon.devineau <simon.devineau@gmail.com>
2012-10-10 09:29:38 +0200

    add methods to ISection

--- EditeurDeTexte/src/model/classes/Section.java
+++ EditeurDeTexte/src/model/classes/Section.java
@@ -1,6 +1,10 @@
 package model.classes;
 
+import java.util.ArrayList;
+
+import model.interfaces.ILine;
 import model.interfaces.ISection;
+import model.interfaces.IText;
 
 /**
  *9 oct. 2012 - EditeurDeTexte.
@@ -10,4 +14,43 @@ import model.interfaces.ISection;
  *Section.java 
  */
 public class Section implements ISection {
+
+    /**
+     * @see model.interfaces.ISection#add(model.interfaces.ISection)
+     */
+    @Override
+    public void add(ISection aSection) {
+    }
+
+    /**
+     * @see model.interfaces.ISection#getParent()
+     */
+    @Override
+    public ISection getParent() {
+        return null;
+    }
+
+    /**
+     * @see model.interfaces.ISection#getChildren()
+     */
+    @Override
+    public ArrayList<ISection> getChildren() {
+        return null;
+    }
+
+    /**
+     * @see model.interfaces.ISection#getTitle()
+     */
+    @Override
+    public ILine getTitle() {
+        return null;
+    }
+
+    /**
+     * @see model.interfaces.ISection#getIntroduction()
+     */
+    @Override
+    public IText getIntroduction() {
+        return null;
+    }
 }
--- EditeurDeTexte/src/model/interfaces/ISection.java
+++ EditeurDeTexte/src/model/interfaces/ISection.java
@@ -1,11 +1,51 @@
 package model.interfaces;
 
+import java.util.ArrayList;
+
 /**
- *9 oct. 2012 - EditeurDeTexte.
- *@author Simon Devineau & Pierre Reliquet
- *Ecole des Mines de Nantes
- *Major in Computer and Information System Engineering
- *ISection.java 
+ * 9 oct. 2012 - EditeurDeTexte.
+ * @author Simon Devineau & Pierre Reliquet
+ *         Ecole des Mines de Nantes
+ *         Major in Computer and Information System Engineering
+ *         ISection.java
  */
 public interface ISection {
+    /**
+     * Add a section to the current section (as a child)
+     * @param aSection
+     *            , the section to add
+     */
+    public void add(ISection aSection);
+
+    /**
+     * @return the parent of the current section.
+     */
+    public ISection getParent();
+
+    /**
+     * @return the children of the current section
+     */
+    public ArrayList<ISection> getChildren();
+
+    /**
+     * @return the section title
+     */
+    public ILine getTitle();
+
+    /**
+     * @return the introduction of the current section
+     */
+    public IText getIntroduction();
+    /**
+     * 
+     * @return the description of the current section.
+     */
+    @Override
+    public String toString();
+    /**
+     * 
+     * @return true if anObject is equal to the current section.
+     */
+    @Override
+    public boolean equals(Object anObject);
 }
