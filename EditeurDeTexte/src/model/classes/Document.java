789a464e848292d7ae1b3ec78c7bb44d0ba7a23a
simon.devineau <simon.devineau@gmail.com>
2012-10-20 14:49:56 +0200

    still coding Document class

--- EditeurDeTexte/src/model/classes/Document.java
+++ EditeurDeTexte/src/model/classes/Document.java
@@ -3,7 +3,11 @@
  */
 package model.classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.interfaces.IDocument;
+import model.interfaces.IText;
 
 /**
  * 10 oct. 2012 - EditeurDeTexte.
@@ -19,14 +23,57 @@ public final class Document // implements IDocument {
     /**
      * The single instance of the document.
      */
-    public static volatile Document documentInstance;
+    // L'utilisation du mot clé volatile permet, en Java version 5 et supérieur,
+    // d'éviter le cas où "Singleton.instance" est non-nul,
+    // mais pas encore "réellement" instancié.
+    private static volatile Document documentInstance;
+    /**
+     * The introduction text of the document
+     */
+    private IText                    introductionText = new Text();
+    /**
+     * The sections list of the document
+     */
+    private List<Section>            sectionsList     = new ArrayList<Section>();
 
     private Document() {
+        introductionText = new Text();
+        sectionsList = new ArrayList<Section>();
     }
 
     public static Document getDocumentInstance() {
         initialize();
         return documentInstance;
+    }
+
+    /**
+     * @return the introductionText
+     */
+    public IText getIntroductionText() {
+        return introductionText;
+    }
+
+    /**
+     * @param introductionText
+     *            the introductionText to set
+     */
+    public void setIntroductionText(IText introductionText) {
+        this.introductionText = introductionText;
+    }
+
+    /**
+     * @return the sectionsList
+     */
+    public List<Section> getSectionsList() {
+        return sectionsList;
+    }
+
+    /**
+     * @param sectionsList
+     *            the sectionsList to set
+     */
+    public void setSectionsList(List<Section> sectionsList) {
+        this.sectionsList = sectionsList;
     }
 
     /**
