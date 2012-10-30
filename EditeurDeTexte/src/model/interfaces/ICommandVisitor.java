a6fd2330bcc9e69bca6f54b70d332b1a06beead4
simon.devineau <simon.devineau@gmail.com>
2012-10-30 08:13:07 +0100

    Add to ICommandVisitor

---
+++ EditeurDeTexte/src/model/interfaces/ICommandVisitor.java
@@ -0,0 +1,33 @@
+package model.interfaces;
+
+/**
+ * 28 oct. 2012 - EditeurDeTexte.
+ * @author Simon Devineau
+ *         Ecole des Mines de Nantes
+ *         Major in Computer and Information System Engineering
+ *         ICommandVisitor.java
+ */
+public interface ICommandVisitor {
+    /**
+     * @param line
+     *            , the line where the commands will be executed.
+     */
+    void visit(ILine line);
+
+    /**
+     * @param section
+     *            , the section where the commands will be executed.
+     */
+    void visit(ISection section);
+
+    /**
+     * @param text
+     *            , the text where the commands will be executed.
+     */
+    void visit(IText text);
+    /**
+     * @param document
+     *            , the text where the commands will be executed.
+     */
+    void visit(IDocument document);
+}
