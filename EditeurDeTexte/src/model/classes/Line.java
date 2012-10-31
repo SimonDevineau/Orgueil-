739fc0605846bbdfdb085b71bc5a56c7101a6a60
Pierre Reliquet <pierre.reliquet@gmail.com>
2012-10-31 18:59:32 +0100

    Implementation of the Left and right commands --

--- EditeurDeTexte/src/model/classes/Line.java
+++ EditeurDeTexte/src/model/classes/Line.java
@@ -1,95 +1,124 @@
 package model.classes;
 
+import java.util.Observable;
+import java.util.Observer;
+
 import model.interfaces.ICommandVisitor;
 import model.interfaces.ILine;
 
 /**
  * 10 oct. 2012 - EditeurDeTexte.
+ * 
  * @author Simon Devineau & Pierre Reliquet Ecole des Mines de Nantes Major in
  *         Computer and Information System Engineering Line.java
  */
-class Line implements ILine {
-    /**
-     * The representation of the line as a StringBuffer to avoid creating a new
-     * string each time that a char is added.
-     */
-    private StringBuilder _Line;
-    private int           _CursorLocation = NO_CURSOR;
+class Line extends Observable implements ILine {
+	/**
+	 * The representation of the line as a StringBuffer to avoid creating a new
+	 * string each time that a char is added.
+	 */
+	private StringBuilder _Line;
+	private int _CursorLocation = NO_CURSOR;
 
-    /**
-     * Default constructor which creates an empty line
-     */
-    Line() {
-        this._Line = new StringBuilder();
-    }
+	/**
+	 * Default constructor which creates an empty line
+	 */
+	Line() {
+		this._Line = new StringBuilder();
+	}
 
-    /**
-     * The constructor to create a line using the CharSequence as starting text.
-     * @param sequence
-     *            , the CharSequence which contains the basis.
-     */
-    Line(CharSequence sequence) {
-        this._Line = new StringBuilder(sequence);
-    }
+	/**
+	 * The constructor to create a line using the CharSequence as starting text.
+	 * 
+	 * @param sequence
+	 *            , the CharSequence which contains the basis.
+	 */
+	Line(CharSequence sequence) {
+		this._Line = new StringBuilder(sequence);
+	}
 
-    @Override
-    public void addUnderCursor(CharSequence insertion) {
-        // TODO vérifier si c'est _CursorLocation ou _CursorLocation+1
-        if (this.hasCursor()) {
-            this._Line.insert(this._CursorLocation, insertion);
-        }
-    }
+	@Override
+	public void addUnderCursor(CharSequence insertion) {
+		// TODO vérifier si c'est _CursorLocation ou _CursorLocation+1
+		if (this.hasCursor()) {
+			this._Line.insert(this._CursorLocation, insertion);
+		}
+	}
 
-    @Override
-    public void append(CharSequence content) {
-        this._Line.append(content);
-    }
+	@Override
+	public void append(CharSequence content) {
+		this._Line.append(content);
+	}
 
-    @Override
-    public void deleteUnderCursor() {
-        if (this.hasCursor()) {
-            this._Line.deleteCharAt(this._CursorLocation);
-        }
-    }
+	@Override
+	public void deleteUnderCursor() {
+		if (this.hasCursor()) {
+			this._Line.deleteCharAt(this._CursorLocation);
+		}
+	}
 
-    @Override
-    public int getCursorLocation() {
-        return this._CursorLocation;
-    }
+	@Override
+	public int getCursorLocation() {
+		return this._CursorLocation;
+	}
 
-    @Override
-    public boolean hasCursor() {
-        return this._CursorLocation != NO_CURSOR;
-    }
+	@Override
+	public boolean hasCursor() {
+		return this._CursorLocation != NO_CURSOR;
+	}
 
-    @Override
-    public void replaceUnderCursor(CharSequence replacement) {
-        if (this.hasCursor()) {
-            // TODO vérifier les index
-            // Copying the start of the line (before the cursor)
-            StringBuilder tmp = new StringBuilder(this._Line.substring(0,
-                    this._CursorLocation));
-            // Adding the replacement
-            tmp.append(replacement);
-            // TODO vérifier les index
-            // Calculating the new cursor location
-            int newCursorLocation = this._CursorLocation + replacement.length();
-            // If the line is longer than the new cursor location we need
-            // to append the rest of the line
-            if (this._Line.length() > newCursorLocation) {
-                tmp.append(this._Line.substring(newCursorLocation));
-            }
-            // We store the new variables.
-            this._Line = tmp;
-            this._CursorLocation = newCursorLocation;
-        }
-    }
+	@Override
+	public void replaceUnderCursor(CharSequence replacement) {
+		if (this.hasCursor()) {
+			// TODO vérifier les index
+			// Copying the start of the line (before the cursor)
+			StringBuilder tmp = new StringBuilder(this._Line.substring(0,
+					this._CursorLocation));
+			// Adding the replacement
+			tmp.append(replacement);
+			// TODO vérifier les index
+			// Calculating the new cursor location
+			int newCursorLocation = this._CursorLocation + replacement.length();
+			// If the line is longer than the new cursor location we need
+			// to append the rest of the line
+			if (this._Line.length() > newCursorLocation) {
+				tmp.append(this._Line.substring(newCursorLocation));
+			}
+			// We store the new variables.
+			this._Line = tmp;
+			this._CursorLocation = newCursorLocation;
+		}
+	}
 
-    /**
-     * @see model.interfaces.IStorable#accept(model.interfaces.ICommandVisitor)
-     */
-    @Override
-    public void accept(ICommandVisitor aVisitor) {
-        aVisitor.visit(this);
-    }
+	/**
+	 * @see model.interfaces.IStorable#accept(model.interfaces.ICommandVisitor)
+	 */
+	@Override
+	public void accept(ICommandVisitor aVisitor) {
+		aVisitor.visit(this);
+	}
+
+	@Override
+	public void setCursorLocation(int position) {
+		if (position != _CursorLocation) {
+			_CursorLocation = position;
+			setChanged();
+			notifyObservers();
+		}
+	}
+
+	@Override
+	public void removeCursor() {
+		setCursorLocation(NO_CURSOR);
+	}
+
+	@Override
+	public synchronized void addObserver(Observer o) {
+		super.addObserver(o);
+	}
+
+	@Override
+	public int length() {
+		return _Line.length();
+	}
 }
--- EditeurDeTexte/src/model/interfaces/ILine.java
+++ EditeurDeTexte/src/model/interfaces/ILine.java
@@ -1,65 +1,97 @@
 package model.interfaces;
 
+import java.util.Observer;
+
 /**
  * 10 oct. 2012 - EditeurDeTexte.
- * @author Simon Devineau & Pierre Reliquet
- *         Ecole des Mines de Nantes
- *         Major in Computer and Information System Engineering
- *         ILine.java
+ * 
+ * @author Simon Devineau & Pierre Reliquet Ecole des Mines de Nantes Major in
+ *         Computer and Information System Engineering ILine.java
  */
 public interface ILine extends IStorable {
-    /**
-     * A constant to indicate that the cursor is not on the line.
-     */
-    public static final int NO_CURSOR = -1;
+	/**
+	 * A constant to indicate that the cursor is not on the line.
+	 */
+	static final int NO_CURSOR = -1;
 
-    /**
-     * Insert the "insertion" param under the cursor if the line has the cursor
-     * @param insertion
-     *            CharSequence, the sequence to insert.
-     */
-    public void addUnderCursor(CharSequence insertion);
+	/**
+	 * Insert the "insertion" param under the cursor if the line has the cursor
+	 * 
+	 * @param insertion
+	 *            CharSequence, the sequence to insert.
+	 */
+	void addUnderCursor(CharSequence insertion);
 
-    /**
-     * @param content
-     *            , content to append.
-     */
-    public void append(CharSequence content);
+	/**
+	 * @param content
+	 *            , content to append.
+	 */
+	void append(CharSequence content);
 
-    /**
-     * Delete the current letter if the line has the cursor
-     */
-    public void deleteUnderCursor();
+	/**
+	 * Delete the current letter if the line has the cursor
+	 */
+	void deleteUnderCursor();
 
-    /**
-     * @param obj
-     *            , the object which should be tester for equality check
-     * @return true if <i><b>this</b></i> and obj are equal
-     */
-    @Override
-    public boolean equals(Object obj);
+	/**
+	 * @param obj
+	 *            , the object which should be tester for equality check
+	 * @return true if <i><b>this</b></i> and obj are equal
+	 */
+	@Override
+	boolean equals(Object obj);
 
-    /**
-     * @return the location of the cursor if the line has the cursor
-     */
-    public int getCursorLocation();
+	/**
+	 * @return the location of the cursor if the line has the cursor
+	 */
+	int getCursorLocation();
 
-    /**
-     * Return true if the current ILine has the cursor and false otherwise
-     * @return a boolean which indicates if the line has the cursor.
-     */
-    public boolean hasCursor();
+	/**
+	 * Set the cursor on a new position on the line.
+	 * 
+	 * @param position
+	 *            , the new position of the cursor.
+	 */
+	void setCursorLocation(int position);
 
-    /**
-     * Replace the following characters by the replacement
-     * @param replacement
-     *            , the new string to insert.
-     */
-    public void replaceUnderCursor(CharSequence replacement);
+	/**
+	 * This method is used to remove the cursor from the current line.
+	 */
+	void removeCursor();
 
-    /**
-     * @return the String representation of the ILine
-     */
-    @Override
-    public String toString();
+	/**
+	 * Return true if the current ILine has the cursor and false otherwise
+	 * 
+	 * @return a boolean which indicates if the line has the cursor.
+	 */
+	boolean hasCursor();
+
+	/**
+	 * Replace the following characters by the replacement
+	 * 
+	 * @param replacement
+	 *            , the new string to insert.
+	 */
+	void replaceUnderCursor(CharSequence replacement);
+
+	/**
+	 * @return the String representation of the ILine
+	 */
+	@Override
+	String toString();
+
+	/**
+	 * The method used to add a new observer on the line. This observer is used
+	 * in order to be able to notify when the line get the cursor.
+	 * 
+	 * @param o
+	 *            , the new Observer
+	 */
+	void addObserver(Observer o);
+
+	/**
+	 * 
+	 * @return the length of the line
+	 */
+	int length();
 }
