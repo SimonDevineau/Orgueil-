486f96fd2ab1aba0e33d22c12745df45580902e5
Pierre Reliquet <pierre.reliquet@gmail.com>
2012-11-02 22:38:25 +0100

    Updating the applejavaextensions

--- EditeurDeTexte/src/model/interfaces/ICommandVisitor.java
+++ EditeurDeTexte/src/model/interfaces/ICommandVisitor.java
@@ -2,12 +2,13 @@ package model.interfaces;
 
 /**
  * 28 oct. 2012 - EditeurDeTexte.
- * @author Simon Devineau
+ * @author Simon Devineau & Pierre Reliquet
  *         Ecole des Mines de Nantes
  *         Major in Computer and Information System Engineering
  *         ICommandVisitor.java
  */
 public interface ICommandVisitor {
+	
     /**
      * @param line
      *            , the line where the commands will be executed.
