0ffd6647bfaa1f3d071dec9d345b8c23064eec30
simon.devineau <simon.devineau@gmail.com>
2012-11-04 03:49:26 +0100

    buffermemory

---
+++ EditeurDeTexte/src/controllers/ManageMemory.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JList;
+
+import model.classes.BufferMemory;
+
+/**
+ * 4 nov. 2012 - EditeurDeTexte.
+ * @author Simon Devineau
+ *         Ecole des Mines de Nantes
+ *         Major in Computer and Information System Engineering
+ *         ManageMemory.java
+ */
+public class ManageMemory implements Observer {
+    private JList list = new JList();
+
+    /**
+     * 
+     */
+    public ManageMemory(JList list) {
+        this.list = list;
+    }
+
+    /**
+     * @see java.util.Observer#update(java.util.Observable, java.lang.Object)
+     */
+    @Override
+    public void update(Observable aArg0, Object aArg1) {
+        list.setListData(BufferMemory.instance().getDocumentStack());
+    }
+}
---
+++ EditeurDeTexte/src/model/classes/commands/InputText.java
@@ -0,0 +1,11 @@
+package model.classes.commands;
+
+/**
+ *3 nov. 2012 - EditeurDeTexte.
+ *@author Simon Devineau
+ *Ecole des Mines de Nantes
+ *Major in Computer and Information System Engineering
+ *InputText.java 
+ */
+public class InputText {
+}
